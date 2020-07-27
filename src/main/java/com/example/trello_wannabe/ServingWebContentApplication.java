6fb65357cab56f5610f1dcb50b98e34a405ab5f2
Gabriel Freire <gabrbfreire@gmail.com>
2020-07-27 20:43:45 -0300

    Initial commit

---
+++ src/main/java/com/example/trello_wannabe/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package com.example.trello_wannabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.example.trello_wannabe.ServingWebContentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trello_wannabe/TrelloWannabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trello_wannabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloWannabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
