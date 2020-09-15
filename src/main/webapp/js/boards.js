1bdbdf09fec70228a0c2510016c6cc31955f0f15
Gabriel Freire <gabrbfreire@gmail.com>
2020-09-15 18:58:47 -0300

    Boards page CRUD

--- src/main/webapp/js/boards.js
+++ src/main/webapp/js/boards.js
@@ -10,26 +10,121 @@ function logout() {
     });
 }
 
-$(document).ready(
-    $(document).ready(function () {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/boards/get",
-            success:function(data) {
-                console.log(data);
-            }
-        })
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/boards/getEmail",
-            success:function(data) {
-                $('#email').text(data);
-                console.log(data);
-            }
-        })
-    })
-)
+$(document).ready(function () {
+
+    getBoards();
+    getUserEmail();
+});
+
+//SELECT
+function getBoards(){
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/boards/get",
+        success:function(data) {
+            buildBoards(data);
+            setDeleteAndUpdateEvents();
+        }
+    });
+}
+
+function getUserEmail(){
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/boards/getEmail",
+        success:function(data) {
+            $('#email').text(data);
+        }
+    });
+}
 
 function buildBoards(data){
-    console.log(data);
+
+    // Removes all boards from page
+    $( ".row" ).empty();
+
+    // Creates the "New Board" button
+    $('.row').append(
+                '<div class="col-md-3" id="new-board-button">'+
+                '<div class="card mb-4 box-shadow">'+
+                '<button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#newBoardModal">New Board</button>'
+    );
+
+    // Creates all boards on page
+    data.forEach( function (board, index) {
+
+        $('#new-board-button').before(
+            '<div class="col-md-3 board">' +
+            '<div class="card mb-4 box-shadow">' +
+            '<div class="card-body">' +
+            '<p class="card-text id="card-text-'+data[index].board_id+'">' + data[index].board_name + '</p>' +
+            '<div class="d-flex justify-content-end">' +
+            '<div class="btn-group" id="'+ data[index].board_id +'">' +
+            '<button type="button" class="btn btn-sm btn-outline-secondary">View</button>' +
+            '<button type="button" class="update-button btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#updateBoardModal">Edit</button>' +
+            '<button type="button" class="delete-button btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#deleteBoardModal">Delete</button>'
+        )
+    });
 }
+
+//CREATE
+$('#new-board-modal-button').on('click', createNewBoard);
+
+function createNewBoard(){
+    let boardName = $('#board-name').val();
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/boards/",
+        data: {boardName: boardName},
+        success:function() {
+            getBoards();
+        }
+    });
+}
+
+// Creates events after the boards are created
+function setDeleteAndUpdateEvents(){
+    let boardId, boardName;
+
+    //DELETE
+    $('.delete-button').on('click', function (){
+
+        boardId = $(this).closest('div').attr('id');
+        console.log(boardId);
+    });
+
+    $('#delete-modal-button').on('click', function (){
+        $.ajax({
+            type: "DELETE",
+            url: "http://localhost:8080/boards",
+            data: {boardId: boardId},
+            success:function() {
+                getBoards();
+            }
+        });
+    });
+
+    //UPDATE
+    $('.update-button').on('click', function (){
+        boardId = $(this).closest('div').attr('id');
+        boardName = $('# card-text-' + boardId).html();
+        console.log('#card-text-' + boardId);
+    });
+
+    $('#update-modal-button').on('click', function (){
+        let boardNewName = $('#board-new-name').val();
+
+        $.ajax({
+            type: "PUT",
+            url: "http://localhost:8080/boards",
+            data: {boardId: boardId, boardNewName: boardNewName},
+            success:function() {
+                getBoards();
+            }
+        });
+    });
+}
+
--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,23 +1,13 @@
-/* function login(email, password) {
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-
-      if (this.status == 200) {
-        window.location.href = 'boards';
-      } else {
-        document.getElementById("result").innerHTML = this.responseText;
-      }
-    }
-  };
-  xhttp.open("POST", "login?email=" + email + "&password=" + password, true);
-  xhttp.send();
-} */
-
+$(document).ready(function () {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/logout"
+  })
+});
 
 $('#login-form').on('submit', function () {
-  let email = $('#user-email').value;
-  let password = $('#user-password').value;
+  let email = $('#user-email').val();
+  let password = $('#user-password').val();
   login(email, password);
   event.preventDefault();
 });
@@ -32,9 +22,10 @@ function login(email, password) {
         window.location.href = 'boards'
       },
       error: function (){
-        console.log('The username or password is incorrect');
         $('#result').text('The username or password is incorrect');
       }
     })
   });
 }
+
+
